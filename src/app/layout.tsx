f62eec7e3525f8325ca9298e60e0b598b0119737
Loai Rebou <loai_rebouh@icloud.com>
2024-10-16 11:26:50 +0100

    adding font styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,36 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Calistoga } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--font-serif",
+});
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+	title: "My Portfolio",
+	description: "Created with love by ME",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={twMerge(
+					inter.className,
+					calistoga.className,
+					"bg-gray-900 text-white antialiased font-sans"
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+	return (
+		<div>
+			<nav>
+				<a href="#home">Home</a>
+				<a href="#projects">Projects</a>
+				<a href="#about">About</a>
+				<a href="#contact">Contact</a>
+			</nav>
+		</div>
+	);
 };
