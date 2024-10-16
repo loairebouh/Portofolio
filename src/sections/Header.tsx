4ac402535b61a8002323e78660728b2a5e468183
Loai Rebou <loai_rebouh@icloud.com>
2024-10-16 11:57:23 +0100

    Styling the Header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,22 @@
 export const Header = () => {
 	return (
-		<div>
-			<nav>
-				<a href="#home">Home</a>
-				<a href="#projects">Projects</a>
-				<a href="#about">About</a>
-				<a href="#contact">Contact</a>
+		<div className="flex justify-center items-center relative top-3">
+			<nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+				<a href="#home" className="nav-item">
+					Home
+				</a>
+				<a href="#projects" className="nav-item">
+					Projects
+				</a>
+				<a href="#about" className="nav-item">
+					About
+				</a>
+				<a
+					href="#contact"
+					className="nav-item bg-white text-gray-900 hover: bg-white/70 "
+				>
+					Contact
+				</a>
 			</nav>
 		</div>
 	);
