d62b36c86c49ea83ddf609f29ae317a098388a0a
Loai Rebou <loai_rebouh@icloud.com>
2024-10-20 07:45:44 +0100

    Complete the auto layout between sm, md and lg devices

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -48,7 +48,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
 	return (
-		<section className="pb-16">
+		<section className="pb-16 lg:py-24">
 			<div className="container">
 				<div className="flex justify-center ">
 					<p className="uppercase font-semibold tracking-widset bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
@@ -56,18 +56,18 @@ export const ProjectsSection = () => {
 					</p>
 				</div>
 
-				<h2 className="font-serif text-3xl text-center mt-6">
+				<h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
 					Feautured Project
 				</h2>
-				<p className="text-center text-white/60 mt-4">
+				<p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
 					See how I turn ideas into captivating and functional digital
 					experiences
 				</p>
-				<div className="flex flex-col mt-10 gap-20">
+				<div className="flex flex-col mt-10 md:mt-20 gap-20">
 					{portfolioProjects.map((project) => (
 						<div
 							key={project.title}
-							className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+							className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none"
 						>
 							<div
 								className="absolute inset-0 -z-10 opacity-5"
@@ -75,35 +75,43 @@ export const ProjectsSection = () => {
 									backgroundImage: `url(${GrainImage.src})`,
 								}}
 							></div>
-							<div className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent inline-flex gap-2 font-bold uppercase tracking-widest text-sm ">
-								<span>{project.company}</span>
-								<span>&bull;</span>
-								<span>{project.year}</span>
+							<div className="lg:grid lg:grid-cols-2 lg:gap-16">
+								<div className="lg:pb-16">
+									<div className="bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent inline-flex gap-2 font-bold uppercase tracking-widest text-sm ">
+										<span>{project.company}</span>
+										<span>&bull;</span>
+										<span>{project.year}</span>
+									</div>
+									<h3 className="font-serif text-2xl md:text-4xl md:mt-5">
+										{project.title}
+									</h3>
+									<hr className="border-t-2 border-white/5 mt-2 md:mt-5" />
+									<ul className="flex flex-col gap-4 mt-4 md:mt-5">
+										{project.results.map((result) => (
+											<li
+												key={project.title}
+												className="flex gap-2 text-sm md:text-base text-white/50"
+											>
+												<CheckBox className="size-5 md:size-6" />
+												<span>{result.title}</span>
+											</li>
+										))}
+									</ul>
+									<a href={project.link}>
+										<button className="bg-white text-gray-950 h-12 w-full md:w-auto      md:px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+											<ArrowUpRight className="size-4" />
+											<span>View Live Project</span>
+										</button>
+									</a>
+								</div>
+								<div className="relative">
+									<Image
+										src={project.image}
+										alt={project.title}
+										className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full  lg:max-w-none lg:w-auto"
+									/>
+								</div>
 							</div>
-							<h3 className="font-serif text-2xl">{project.title}</h3>
-							<hr className="border-t-2 border-white/5 mt-2" />
-							<ul className="flex flex-col gap-4 mt-4">
-								{project.results.map((result) => (
-									<li
-										key={project.title}
-										className="flex gap-2 text-sm text-white/50"
-									>
-										<CheckBox className="size-5" />
-										<span>{result.title}</span>
-									</li>
-								))}
-							</ul>
-							<a href={project.link}>
-								<button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-									<ArrowUpRight className="size-4" />
-									<span>View Live Project</span>
-								</button>
-							</a>
-							<Image
-								src={project.image}
-								alt={project.title}
-								className="mt-8 -mb-4"
-							/>
 						</div>
 					))}
 				</div>
