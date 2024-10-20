1b6cccea3d02fb1337010475005ddc79e9403461
Loai Rebou <loai_rebouh@icloud.com>
2024-10-20 17:16:04 +0100

    Styling the testimonial section and fixing the auto layout

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,28 +3,29 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import yasserPic from "@/assets/images/yasser-moula.png";
+import ayodaPic from "@/assets/images/abdallah-mehiz.png";
 import Image from "next/image";
 import { SectionHeader } from "@/components/SectionHeader";
-import grainImage from "@/assets/images/grain.jpg";
 import { Card } from "@/components/Card";
 
 const testimonials = [
 	{
-		name: "Alex Turner",
-		position: "Marketing Manager @ TechStartups",
-		text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
-		avatar: memojiAvatar1,
+		name: "Yasser Moula",
+		position: "E-com Owner @ Ys Shop",
+		text: "All the technical aspects of my business, Ys Shop, rely on Loai. He’s incredibly helpful and always provides valuable advice while ensuring the job is done efficiently. His attention to detail and deep understanding of our brand are outstanding. We're absolutely delighted with the results!",
+		avatar: yasserPic,
 	},
 	{
-		name: "Olivia Green",
-		position: "Head of Design @ GreenLeaf",
-		text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
-		avatar: memojiAvatar2,
+		name: "Abdallah Mehiz",
+		position: "CEO & Mobile Dev @ Qotra",
+		text: "A Great dev in the making You're on an impressive path! Your work as a developer and your eye for design are coming together in a powerful way. Keep it up!",
+		avatar: ayodaPic,
 	},
 	{
-		name: "Daniel White",
-		position: "CEO @ InnovateCo",
-		text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+		name: "Bassem Djeddi",
+		position: "Merchant @ DjeddiFurniture",
+		text: "",
 		avatar: memojiAvatar3,
 	},
 	{
@@ -43,7 +44,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
 	return (
-		<div className="py-16">
+		<div className="py-16 lg:py-24">
 			<div className="container">
 				<SectionHeader
 					title="What People Say About Me"
@@ -51,17 +52,38 @@ export const TestimonialsSection = () => {
 					descreption="Dont just take my word for it, See what my clients have to say about my
 				work"
 				/>
-
-				<div>
-					{testimonials.map((testimonial) => (
-						<Card key={testimonial.name}>
-							<div className="flex ">
-								<Image src={testimonial.avatar} alt="client pic" />
-								<div>{testimonial.position}</div>
-								<p>{testimonial.text}</p>
-							</div>
-						</Card>
-					))}
+				<div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+					<div className="flex gap-8 flex-none">
+						{testimonials.map((testimonial) => (
+							<Card
+								key={testimonial.name}
+								className="max-w-xs md:max-w-md md:p-8"
+							>
+								<div className="flex gap-4 items-center">
+									<div
+										className={
+											"size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0"
+										}
+									>
+										<Image
+											src={testimonial.avatar}
+											alt="client pic"
+											className={"mt-5"}
+										/>
+									</div>
+									<div className="">
+										<div className={"font-semibold"}>{testimonial.name}</div>
+										<div className={"text-sm text-white/40"}>
+											{testimonial.position}
+										</div>
+									</div>
+								</div>
+								<p className={"mt-4 text-sm md:text-base "}>
+									{testimonial.text}
+								</p>
+							</Card>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
